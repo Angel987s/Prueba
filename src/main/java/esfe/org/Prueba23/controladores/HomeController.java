7b7212e6459f0828a5bab90c2c6487def013d345
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-08-25 22:08:15 -0600

    home

---
+++ src/main/java/esfe/org/Prueba23/controladores/HomeController.java
@@ -0,0 +1,14 @@
+package esfe.org.Prueba23.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public RedirectView redirectToMarcas() {
+        return new RedirectView("/productos");
+    }
+
+}
